e8e5d4a14959331bdb5e91fcdb990e30edd5a2b7
Ruth <ruthjohnstoncodes@gmail.com>
2019-03-25 20:05:38 +0000

    created seeds file for sample data

--- config/application.rb
+++ config/application.rb
@@ -19,15 +19,13 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    # customises what the generator generates!
+    config.generators do |g|
+        g.orm               :active_record
+        g.template_engine   :erb
+        g.test_framework    :rspec # generates a series of RSpec tests (didn't generate them this time round)
+        g.stylesheets       :false
+        g.javascripts       :false
+    end
   end
 end
